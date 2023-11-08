984f4ac04031caec3b52cb77eccf1f477440a237
KKamtte <swye199@gmail.com>
2023-11-09 00:45:35 +0900

    Author relation

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,12 +1,17 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class PostsModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  author: string;
+  // 1) UsersModel 과 연결 (Foreign Key)
+  // 2) null 이 될 수 없다
+  @ManyToOne(() => UsersModel, (user) => user.posts, {
+    nullable: false,
+  })
+  author: UsersModel;
 
   @Column()
   title: string;
