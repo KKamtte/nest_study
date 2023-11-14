755fc9186a32da63acf88b1bfb60bda275d00142
KKamtte <swye199@gmail.com>
2023-11-14 23:09:21 +0900

    login logic 정리

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  /**
+   * 만들 기능
+   *
+   * 1) registerWithEmail
+   *    - email, nickname, password 를 입력 받고 사용자를 생성한다.
+   *    - 생성이 완료되면 accessToken 과 refreshToken 을 반환한다.
+   *    - 회원 가입 후 다시 로그인 하는 과정을 넣지 않음.
+   *
+   * 2) loginWithEmail
+   *    - email, password 를 입력하면 사용자 검증을 진행한다.
+   *    - 검증이 완료되면 accessToken 과 refreshToken 을 반환한다.
+   *
+   * 3) loginUser
+   *    - (1) 과 (2) 에서 필요한 accessToken 과 refreshToken 을 반환하는 로직
+   *
+   * 4) signToken
+   *    - (3) 에서 필요한 accessToken 과 refreshToken 을 sign 하는 로직
+   *
+   * 5) authenticateWithEmailAndPassword
+   *    - (2) 에서 로그인을 진행할 때 필요한 기본적인 검증 진행
+   *    - 1. 사용자가 존재하는지 확인 (email)
+   *    - 2. 비밀번호가 맞는지 확인
+   *    - 3. 모두 통과되면 찾은 사용자 정보 반환
+   *    - 4. loginWithEmail 에서 반환된 데이터를 기반으로 토큰 생성
+   */
+}
