80f27c95ddb4db243d63562045ea5e3ee4d2a643
KKamtte <swye199@gmail.com>
2023-12-06 23:56:00 +0900

    여러개 Pipe 사용하기

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -18,8 +18,44 @@ export class PasswordPipe implements PipeTransform {
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: any, metadata: ArgumentMetadata) {
-    if (value.length > 8) {
+    if (value.toString().length > 8) {
       throw new BadRequestException('비밀번호는 8자 이하로 입력해야함');
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+  constructor(
+    private readonly length: number,
+    private readonly subject: string,
+  ) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length > this.length) {
+      throw new BadRequestException(
+        `${this.subject}의 최대 길이는 ${this.length} 입니다.`,
+      );
+    }
+
+    return value.toString();
+  }
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+  constructor(
+    private readonly length: number,
+    private readonly subject: string,
+  ) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (value.toString().length < this.length) {
+      throw new BadRequestException(
+        `${this.subject}의 최소 길이는 ${this.length} 입니다.`,
+      );
     }
 
     return value.toString();
