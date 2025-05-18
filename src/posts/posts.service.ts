784e0fa947bc1e422598ec6e55a8639870a44ba0
KKamtte <swye199@gmail.com>
2025-05-18 12:06:11 +0900

    내림차순 정렬

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { MoreThan, Repository } from 'typeorm';
+import { FindOptionsWhere, LessThan, MoreThan, Repository } from 'typeorm';
 import { PostsModel } from './entities/posts.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -31,10 +31,14 @@ export class PostsService {
 
   // 1) 오름차순으로 정렬하는 pagination만 구현
   async paginatePosts(dto: PaginatePostsDto) {
+    const where: FindOptionsWhere<PostsModel> = {};
+    if (dto.where__id_less_than) {
+      where.id = LessThan(dto.where__id_less_than);
+    } else if (dto.where__id_more_than) {
+      where.id = MoreThan(dto.where__id_more_than);
+    }
     const posts = await this.postsRepository.find({
-      where: {
-        id: MoreThan(dto.where__id_more_than ?? 0),
-      },
+      where,
       order: {
         createAt: dto.order__createdAt,
       },
